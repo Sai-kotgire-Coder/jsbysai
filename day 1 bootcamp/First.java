89270da07d5e757c193b1d94927f71f1583e4bb4
saikotgire <saikotgire9@gmail.com>
2023-09-16 01:17:33 +0530

    initial push

---
+++ day 1 bootcamp/First.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+class First 
+{
+public static void main (String args[])    
+{
+    System.out.println("hello this is first program");
+    int x=5;
+    int y=8;
+    int z=x+y;
+    System.out.println("addition is "+z);
+}
+}
